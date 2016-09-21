f85a16924865e8e2fe139d2175ac210f5c5a3084
神楽坂碧玺 <0xbbc@0xbbc.com>
2016-09-22 03:34:57 +0800

    Setting presentation style doesn't work, now a class var 'isWindowShown' is used as a workaround

--- Maria/SettingsWindowController.swift
+++ Maria/SettingsWindowController.swift
@@ -8,11 +8,24 @@
 
 import Cocoa
 
-class SettingsWindowController: NSWindowController {
+class SettingsWindowController: NSWindowController, NSWindowDelegate {
+    static var isWindowShown = false
+    
+    func windowWillClose(_ aNotification: Notification) {
+        SettingsWindowController.isWindowShown = false
+    }
+    
+    override func showWindow(_ sender: Any?) {
+        if !SettingsWindowController.isWindowShown {
+            super.showWindow(sender)
+            SettingsWindowController.isWindowShown = true
+        }
+    }
     
     override func windowDidLoad() {
         super.windowDidLoad()
         NSApp.activate(ignoringOtherApps: true)
         self.window?.canHide = false
+        self.window?.delegate = self
     }
 }
